d3f34263679183939176c2589cdca3524f61fe2a
Ellen Shin <ellenshin1001@gmail.com>
2017-08-01 11:18:55 -0700

    no retweets

--- flask-app.py
+++ flask-app.py
@@ -93,7 +93,7 @@ def get_data():
     listofcid = []
     prev_obj = None
 
-    with open('Smaller_Data_Set_Posts14.csv', errors='ignore') as csvfile:
+    with open('Large_Data_Posts_NoRetweets.csv', errors='ignore') as csvfile:
         reader = csv.reader(csvfile)
         contents = [row for row in reader]
 
