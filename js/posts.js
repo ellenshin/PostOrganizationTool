64cd90c00047acbcb878ccdbc5246fa035f035be
Ellen Shin <ellenshin1001@gmail.com>
2017-08-02 17:19:43 -0700

    style.visibility

--- js/posts.js
+++ js/posts.js
@@ -19,9 +19,9 @@ var before_id_btn = document.getElementById("before_id")
 var after_id_btn = document.getElementById("after_id")
 
 if before_id_btn.getAttribute("href") == "//0" {
-    before_id_btn.visibility = "hidden"
+    before_id_btn.style.visibility = "hidden"
 }
 
 if after_id_btn.getAttribute("href") == "//0" {
-    after_id_btn.visibility = "hidden"
+    after_id_btn.style.visibility = "hidden"
 }
